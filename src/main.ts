060d6fea5371bd4eed338436c340a01535b77967
Wygor Fonseca <wygorfonseca@hotmail.com>
2026-02-03 14:12:25 -0300

    fix: webhook raw

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
 
-  app.use('/checkout/webhook', express.raw({ type: 'application/json' }));
+  // RAW apenas no webhook do Stripe (path exato!)
+  app.use('/checkout/webhook', express.raw({ type: '*/*' }));
+
+  // JSON para o resto
+  app.use(express.json());
 
   await app.listen(process.env.PORT ?? 3000);
 }
